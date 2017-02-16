e5b810cb36124deb0e649afd58f8f8479deebec7
Thomas Sherwood <tom0521@optonline.net>
2017-02-16 17:05:12 -0500

    More Queue functionality including adding songs to the Queue and getting the next song in the Queue; Added getQueue() method.

--- src/jukebox/data/Playlist.java
+++ src/jukebox/data/Playlist.java
@@ -31,4 +31,23 @@ public class Playlist extends ArrayList<String> {
         return true;
     }
 
+    public boolean addToQueue(int index) {
+
+        if(index < 0 || index > size())
+            return false;
+
+        queue.add(this.get(index));
+        return true;
+    }
+
+    // Removes the current song from the queue and return the next song to be played
+    public String next(){
+        queue.remove(0);
+        return queue.get(0);
+    }
+
+    public ArrayList<String> getQueue(){
+        return queue;
+    }
+
 }
--- tests/PlaylistTest.java
+++ tests/PlaylistTest.java
@@ -1,4 +1,5 @@
 import jukebox.data.Playlist;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -10,14 +11,39 @@ import static org.junit.Assert.*;
  */
 public class PlaylistTest {
 
+    private Playlist playlist;
+
+    @Before
+    public void initializeTests(){
+        playlist = new Playlist();
+        playlist.add("Banana"); playlist.add("apple"); playlist.add("GraPE");
+    }
+
     @Test
     public void playlistSortTest(){
         ArrayList<String> sorted = new ArrayList<>();
         sorted.add("apple"); sorted.add("Banana"); sorted.add("GraPE");
 
-        Playlist playlist = new Playlist();
-        playlist.add("Banana"); playlist.add("apple"); playlist.add("GraPE");
-
         assertEquals(sorted, playlist);
+    }
+
+    @Test
+    public void addToQueueTest(){
+
+        assertTrue(playlist.addToQueue(0));
+        assertFalse(playlist.addToQueue(4));
+
+        playlist.addToQueue(1);
+        playlist.addToQueue(2);
+
+        assertEquals(playlist, playlist.getQueue());
+    }
+
+    @Test
+    public void nextTest(){
+        playlist.addToQueue(0);
+        playlist.addToQueue(1);
+
+        assertEquals("Banana", playlist.next());
     }
 }
